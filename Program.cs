596887ff2eeafa4536ae7892772271dea7e970a3
Linus Nagel <linus_nagel@outlook.de>
2023-04-10 22:53:05 +0200

    Created a Console Application

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Debugger.Console console = Debugger.Console.Instaciate;
+        console.Log("Test");
+
+        NeatImplementation.NEAT neat = new NeatImplementation.NEAT(3, 3, 10);
+        for (int i = 0; i < 1000; i++) {
+            neat.
+        }
+        
+        Console.ReadKey();
+    }
+}
