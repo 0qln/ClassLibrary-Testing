e0e01e3ffc246817234aa4513332355f7562d26e
Linus Nagel <linus_nagel@outlook.de>
2023-04-10 23:01:17 +0200

    Started creating the testing enviroment

--- Program.cs
+++ Program.cs
@@ -7,8 +7,9 @@
 
         NeatImplementation.NEAT neat = new NeatImplementation.NEAT(3, 3, 10);
         for (int i = 0; i < 1000; i++) {
-
+            neat.NextGeneration();
         }
+        float[] output = neat.FeedForward(new float[] {1, 1, 1}, neat.BestGenome());
         
         Console.ReadKey();
     }
