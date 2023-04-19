4ef4d22b68a591ca32bbf6ce5ddd028d582786c8
Linus Nagel <129159411+0qln@users.noreply.github.com>
2023-04-19 23:03:47 +0200

    .

--- Program.cs
+++ Program.cs
@@ -3,14 +3,7 @@
     static void Main(string[] args)
     {
         Debugger.Console console = Debugger.Console.Instaciate;
-        console.Log("Test");
-
-        /*
-        NeatImplementation.NEAT neat = new NeatImplementation.NEAT(3, 3, 10);
-        for (int i = 0; i < 1000; i++) {
-            neat.NextGeneration();
-        }
-        float[] output = neat.FeedForward(new float[] {1, 1, 1}, neat.BestGenome());
-        */
+        Console.WriteLine(console.ApplicationPath);
+        
     }
 }
