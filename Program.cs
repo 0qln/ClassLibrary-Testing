a195f0da0c6e99c030064a30005bee1dd0416deb
Linus Nagel <129159411+0qln@users.noreply.github.com>
2023-04-21 18:09:11 +0200

    .

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@
     static void Main(string[] args)
     {
         Debugger.Console console = Debugger.Console.Instaciate;
-        Console.WriteLine(console.ApplicationPath);
+        console.Log("");
         
     }
 }
